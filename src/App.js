56b27c1523ff30b16de02c16fee610bc67c6663b
Pins Thoo <aipeng.thoo@gmail.com>
2020-07-18 21:54:05 +0800

    added router components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import './App.css';
 import About from './About';
 import Nav from './Nav';
 import Shop from './Shop';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Nav />
-      <About />
-      <Shop />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop} />
+      </Switch>
     </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
 
 export default App;
--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
     <nav>
-      <h3>Logo</h3>
+      <Link to="/" className="nav-inner-link">
+        <h3>Logo</h3>
+      </Link>
       <ul className="nav-links">
-        <li>About</li>
-        <li>Shop</li>
+        <Link to="/about" className="nav-inner-link">
+          <li>About</li>
+        </Link>
+        <Link to="/shop" className="nav-inner-link">
+          <li>Shop</li>
+        </Link>
+        
       </ul>
     </nav>
   );
