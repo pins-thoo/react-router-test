39ed72078c187ca61d442fa04059fbbf873c7113
Pins Thoo <aipeng.thoo@gmail.com>
2020-07-18 22:56:17 +0800

    added shop api

--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+const FortniteAPI = require("fortnite-api-io");
+const fortniteAPI= new FortniteAPI("5be289e3-b77ce869-98c3fe44-e3165ec1");
+
+
+
 
 function Shop() {
+
+  useEffect(() => {
+    upcomingItems();
+  },[]);
+
+  const [items, setItems] = useState([]);
+
+  const upcomingItems = async () => {
+    const items = await fortniteAPI.listUpcomingItems();
+    console.log(items);
+    setItems(items.items);
+  };
+
   return (
+    
     <div>
-      <h1>Shop Page</h1>
+      {items.map(item => (
+        <h1 key={item.id}>{item.name}</h1>
+      ))}
     </div>
   );
 }
